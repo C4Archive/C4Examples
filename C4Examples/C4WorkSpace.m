a8485714a77ecbe8c032b48338cdc794ff755982
-- C4 -- <travis@c4ios.com>
2014-06-17 17:13:17 -0700

    Adding first listen example

--- C4Examples/C4WorkSpace.m
+++ C4Examples/C4WorkSpace.m
@@ -7,10 +7,37 @@
 
 #import "C4WorkSpace.h"
 
-@implementation C4WorkSpace
+@implementation C4WorkSpace{
+    C4Shape *s1, *s2;
+}
 
 -(void)setup {
-    //work your magic here
+    CGRect shapeFrame = CGRectMake(0, 0, 100, 100);
+    s1 = [C4Shape ellipse:shapeFrame];
+    s2 = [C4Shape ellipse:shapeFrame];
+    CGPoint centerPoint = self.canvas.center;
+    centerPoint.y -= 75;
+    s1.center = centerPoint;
+    centerPoint.y += 150;
+    s2.center = centerPoint;
+    
+    [s1 onTap:^(CGPoint location) {
+        [s1 tapped:location];
+    }];
+    
+    [s2 onTap:^(CGPoint location) {
+        [s2 tapped:location];
+    }];
+        
+    [self listenFor:@"tapped" andRun:^(NSNotification *n) {
+        C4Shape *shape = (C4Shape *)n.object;
+        shape.fillColor = [UIColor colorWithRed:[C4Math randomInt:100]/100.0f
+                                          green:[C4Math randomInt:100]/100.0f
+                                           blue:[C4Math randomInt:100]/100.0f
+                                          alpha:1.0f];
+    }];
+    
+    [self.canvas addObjects:@[s1, s2]];
 }
 
 @end
---
+++ C4Examples/Code/Listen/Listen01.m
@@ -0,0 +1,17 @@
+//
+//  Listen01.m
+//  C4Examples
+//
+//  Created by travis on 2014-06-17.
+//  Copyright (c) 2014 C4. All rights reserved.
+//
+
+#import "Listen01.h"
+
+@implementation Listen01
+
+-(void)setup {
+    //work your magic here
+}
+
+@end
